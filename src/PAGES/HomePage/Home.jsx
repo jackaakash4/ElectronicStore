e043d285b46b61b8ad807363086201fb3e36e1ce
Jack Aakash <jackaakash4@gmail.com>
2023-07-05 00:03:33 +0545

    Creating navbar

---
+++ src/PAGES/HomePage/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../COMPONENTS/Navbar/Navbar'
+
+function Home() {
+  return (
+    <div>
+        <nav>
+            <Navbar />
+        </nav>
+    </div>
+  )
+}
+
+export default Home
